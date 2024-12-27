6bafbd51e542bda7403eb057fd86669a31f36698
jeewantha <upuljeewantha070@gmail.com>
2024-12-27 22:13:06 +0530

    implemented the layout and the data fetching in the menu route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./UI/Home";
 import Menu from "./features/menu/Menu";
 import CreateOrder from "./features/orders/CreateOrder";
 import Order from "./features/orders/Order";
+import Cart from "./features/cart/Cart";
 import AppLayout from "./UI/AppLayout";
 import { loader as menuLoader } from "./features/menu/Menu";
 
@@ -16,6 +17,7 @@ const App = () => {
         { path: "/menu", element: <Menu />, loader: menuLoader },
         { path: "/order/new", element: <CreateOrder /> },
         { path: "/order/:orderId", element: <Order /> },
+        { path: "/cart", element: <Cart /> },
       ],
     },
   ]);
