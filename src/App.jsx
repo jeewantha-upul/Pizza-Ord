f97b5e531bed6b47ac86665f65db92c7e032535f
jeewantha <upuljeewantha070@gmail.com>
2024-12-30 11:03:24 +0530

    get the order from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./UI/Home";
+import Error from "./UI/Error";
 import Menu from "./features/menu/Menu";
 import CreateOrder from "./features/orders/CreateOrder";
 import Order from "./features/orders/Order";
 import Cart from "./features/cart/Cart";
 import AppLayout from "./UI/AppLayout";
 import { loader as menuLoader } from "./features/menu/Menu";
+import { loader as orderLoader } from "./features/orders/Order";
 
 const App = () => {
   const routes = createBrowserRouter([
     {
       element: <AppLayout />,
+      errorElement: <Error />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "/menu", element: <Menu />, loader: menuLoader },
+        {
+          path: "/menu",
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
         { path: "/order/new", element: <CreateOrder /> },
-        { path: "/order/:orderId", element: <Order /> },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />,
+        },
         { path: "/cart", element: <Cart /> },
       ],
     },
